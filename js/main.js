a5e9de159dacbeb57546be79fbbbce9b3020cc0c
SantiGennu <santigennu5@gmail.com>
2022-07-14 18:29:52 +0200

    primera entrega final

--- js/main.js
+++ js/main.js
@@ -1,151 +1,193 @@
-
-/*                          ///CALCULAR PRECIOS CON DESCUENTO E IVA//
-
-const suma = (a , b , c) => a + b + c
-const resta = (a , b) => a - b 
-
-const iva = c => c * 0.25
-let precioAereo = 350
-let descuento = 49.99
-let premium = 99.99
-
-let precioFinal = resta (suma(precioAereo , clasePremium , iva (precioAereo)) , descuento)
-console.log(precioFinal)
-*/
-
-/*
-const nombre = prompt("Ingrese nombre y apellido")
-const suma = (a , b , c) => a + b + c
-const resta = (a , b) => a - b 
-const iva = x => x * 0.25
-let precioAereo = 0
-const descuento = 49.99
-const clasePremium = 99.99
-const cero = 0
-let precioFinal
-
-alert ("Hola  " + nombre + ",  Bienvenido a Gate05")
-
-let destino = prompt("Ingrese el destino al que desea viajar (Roma, Londres, Paris)").toLowerCase()
-
-while (destino != "roma" && destino != "londres" && destino != "paris" ){
-    alert("El destino no existe")
-    destino = prompt("Ingrese el destino al que desea viajar (Roma, Londres, Paris)").toLowerCase()
-}
-if (destino == "roma"){
-    precioAereo = 350
-}
-else if(destino == "londres"){
-    precioAereo = 375
-}
-else{
-    precioAereo = 400
-}
-
-let clase = prompt("desea viajar en clase premium? (SI = premium / NO = economic)").toLowerCase()
-
-while(clase != "si" && clase != "no"){
-    alert("respuesta incorrecta")
-    clase = prompt("desea viajar en clase premium? (SI = premium / NO = economic)").toLowerCase()
-}
-
-if (clase == "si"){
-    precioFinal = resta (suma(precioAereo , clasePremium , iva (precioAereo)) , descuento)
-    alert("el precio final es de  " + precioFinal)
-}
-
-else{
-    precioFinal = resta (suma(precioAereo, iva (precioAereo), cero) , descuento)
-    alert("el precio final es de  " + precioFinal)
-}
-*/
-                //AGREGAR DESTINOS Y PUSHEARLOS SI NO ESTAN EN LA LISTA//
-
-// let destino = ["roma", "londres", "paris", "amsterdam", "bruselas", "viena", "estocolmo", "praga", "varsovia", "lisboa", "oslo"]
-
-// console.log(destino);
-
-// let destinoNuevo = prompt("elija un destino europeo y les buscaremos las mejores ofertas")
-// if (destino.includes (destinoNuevo)){
-//     alert("El destino ya esta se encuentra en la lista")
-//     destinoNuevo = prompt("elija un destino europeo y les buscaremos las mejores ofertas")
-// }
-
-// else (destino.indexOf (destinoNuevo) == -1) ; {
-//     destino.push (destinoNuevo);
-//     console.log (destino);
-
-// }
-
-                        //ELIMINAR DESTINOS//
-
-// let destino = ["roma", "londres", "paris", "amsterdam", "bruselas", "viena", "estocolmo", "praga", "varsovia", "lisboa", "oslo"]
-
-// const eliminar = prompt ("que ciudad desea eliminar de la lista?") 
-// let index = destino.indexOf(eliminar)
-// if (index != -1) {
-//     destino.splice (index, 1)
-  
-// }
-// console.log(destino)
-
-                            //arrays + objetos (for...of)
-
-class destinos {
-    constructor(ciudad, precio){
+class Destinos {
+    constructor(ciudad, precio) {
         this.ciudad = ciudad;
         this.precio = precio;
 
     }
-  
+
 }
-let listaDestinos = [
-    {ciudad: "roma", precio: 500 },
-    {ciudad: "londres", precio: 550 },
-    {ciudad: "paris", precio: 400 },
-    {ciudad: "amsterdam", precio: 470 },
-    {ciudad: "bruselas", precio: 560 },
-    {ciudad: "viena", precio: 580 },
-    {ciudad: "estocolmo", precio: 520 },
-    {ciudad: "praga", precio: 480 },
-    {ciudad: "varsovia", precio: 390 },
-    {ciudad: "lisboa", precio: 510 },
-    {ciudad: "oslo", precio: 600 },
+const destino1 = new Destinos("roma", 500)
+const destino2 = new Destinos("londres", 550)
+const destino3 = new Destinos("paris", 400)
+const destino4 = new Destinos("amsterdam", 470)
+const destino5 = new Destinos("bruselas", 560)
+const destino6 = new Destinos("viena", 580)
+const destino7 = new Destinos("estocolmo", 520)
+const destino8 = new Destinos("praga", 480)
+const destino9 = new Destinos("varsovia", 390)
+const destino10 = new Destinos("lisboa", 510)
+const destino11 = new Destinos("oslo", 600)
 
-];
-for (let destinos of listaDestinos)
-{
-    console.log(`su destino es ${destinos.ciudad} y su precio es de ${destinos.precio}`);
+
+
+let listaDestinos = [destino1 , destino2, destino3, destino4, destino5, destino6, destino7, 
+    destino8, destino9, destino10, destino11 ];
+
+const descuento = 49.99
+const descuento2 = 99.99
+const descuento3 = 149.99
+const clasePremium = 99.99
+
+
+
+const compra = []
+const nombre = prompt("Ingrese nombre y apellido")
+alert("Hola  " + nombre + ",  Bienvenido a Gate05")
+
+let ciudad = prompt("ingrese un destino europeo al que quiera visitar").toLocaleLowerCase()
+let find = listaDestinos.find(destino => destino.ciudad == ciudad)
+console.log(find)
+
+while( ciudad != "roma" && ciudad != "londres" && ciudad != "paris" 
+&& ciudad != "amsterdam" && ciudad != "bruselas"
+&& ciudad != "estocolmo" && ciudad != "viena" && ciudad != "praga"
+&& ciudad != "varsovia" && ciudad != "lisboa" && ciudad != "oslo"){
+alert("no hay vuelos disponibles a ese destino")
+    ciudad = prompt("ingrese un destino europeo al que quiera visitar").toLowerCase()}
+if (ciudad == "roma") {
+    compra.push(destino1)
+    alert("Genial! usted esta a un pasito de subirse al avion")
+}
+else if (ciudad == "londres") {
+    compra.push(destino2)
+    alert("Genial! usted esta a un pasito de subirse al avion")
+}
+else if (ciudad == "paris") {
+    compra.push(destino3)
+    alert("genial! usted esta a un pasito de subirse al avion")
+}
+else if (ciudad == "amsterdam") {
+    compra.push(destino4)
+    alert("genial! usted esta a un pasito de subirse al avion")
+}
+else if (ciudad == "bruselas") {
+    compra.push(destino5)
+    alert("genial! usted esta a un pasito de subirse al avion")
+}
+else if (ciudad == "viena") {
+    compra.push(destino6)
+    alert("Genial! usted esta a un pasito de subirse al avion")
+}
+else if (ciudad == "estocolmo") {
+    compra.push(destino7)
+    alert("Genial! usted esta a un pasito de subirse al avion")
+}
+else if (ciudad == "praga") {
+    compra.push(destino8)
+    alert("Genial! usted esta a un pasito de subirse al avion")
+}
+else if (ciudad == "varsovia") {
+    compra.push(destino9)
+    alert("genial! usted esta a un pasito de subirse al avion")
+}
+else if (ciudad == "lisboa") {
+    compra.push(destino10)
+    alert("genial! usted esta a un pasito de subirse al avion")
+}
+else if (ciudad == "oslo") {
+    compra.push(destino11)
+    alert("Genial! usted esta a un pasito de subirse al avion")
+} else {
+    alert("no hay vuelos disponibles a ese destino")
+    ciudad = prompt("ingrese un destino europeo al que quiera visitar").toLowerCase()
+}
+let pack = prompt("desea elegir otro destino y aprovechar los mejores descuentos? (SI / NO)").toLowerCase()
+while (pack != "si" && pack != "no") {
+    alert("respuesta incorrecta")
+    pack = prompt("desea elegir otro destino y aprovechar los mejores descuentos? (SI / NO)").toLowerCase()
+}
+
+if (pack == "si") {
+    ciudad = prompt("ingrese otro destino europeo al que quiera visitar").toLowerCase()
+    
+    while( ciudad != "roma" && ciudad != "londres" && ciudad != "paris" 
+    && ciudad != "amsterdam" && ciudad != "bruselas"
+    && ciudad != "estocolmo" && ciudad != "viena" && ciudad != "praga"
+    && ciudad != "varsovia" && ciudad != "lisboa" && ciudad != "oslo"){
+    alert("no hay vuelos disponibles a ese destino")
+        
+    ciudad = prompt("ingrese un destino europeo al que quiera visitar").toLowerCase()}
+    if (ciudad == "roma") {
+        compra.push(destino1)
+        alert("Genial! usted esta a un pasito de subirse al avion")
+    }
+    else if (ciudad == "londres") {
+        compra.push(destino2)
+        alert("Genial! usted esta a un pasito de subirse al avion")
+    }
+    else if (ciudad == "paris") {
+        compra.push(destino3)
+        alert("genial! usted esta a un pasito de subirse al avion")
+    }
+    else if (ciudad == "amsterdam") {
+        compra.push(destino4)
+        alert("genial! usted esta a un pasito de subirse al avion")
+    }
+    else if (ciudad == "bruselas") {
+        compra.push(destino5)
+        alert("genial! usted esta a un pasito de subirse al avion")
+    }
+    else if (ciudad == "viena") {
+        compra.push(destino6)
+        alert("Genial! usted esta a un pasito de subirse al avion")
+    }
+    else if (ciudad == "estocolmo") {
+        compra.push(destino7)
+        alert("Genial! usted esta a un pasito de subirse al avion")
+    }
+    else if (ciudad == "praga") {
+        compra.push(destino8)
+        alert("Genial! usted esta a un pasito de subirse al avion")
+    }
+    else if (ciudad == "varsovia") {
+        compra.push(destino9)
+        alert("genial! usted esta a un pasito de subirse al avion")
+    }
+    else if (ciudad == "lisboa") {
+        compra.push(destino10)
+        alert("genial! usted esta a un pasito de subirse al avion")
+    }
+    else if (ciudad == "oslo") {
+        compra.push(destino11)
+        alert("Genial! usted esta a un pasito de subirse al avion")
+    } else {
+        alert("este destino no existe")
+        ciudad = prompt("ingrese otro destino europeo al que quiera visitar").toLowerCase()
+    }
+
+    let clase = prompt("desea viajar en clase premium? (SI = premium / NO = economic)").toLowerCase()
+    while (clase != "si" && clase != "no") {
+        alert("respuesta incorrecta")
+        clase = prompt("desea viajar en clase premium? (SI = premium / NO = economic)").toLowerCase()
+    }
+    if (clase == "si") {
+       
+        const precioTotal = compra.reduce((acc, el) => acc + el.precio + clasePremium - descuento, 0)
+        alert("el valor de tu vuelo es de " + Math.round(precioTotal))
+    } else {
+       
+        const precioTotal = compra.reduce((acc, el) => acc + el.precio - descuento, 0)
+        alert("el valor de tu vuelo es de " + Math.round(precioTotal))
+    }
+} else {
+
+    let clase = prompt("desea viajar en clase premium? (SI = premium / NO = economic)").toLowerCase()
+    while (clase != "si" && clase != "no") {
+        alert("respuesta incorrecta")
+        clase = prompt("desea viajar en clase premium? (SI = premium / NO = economic)").toLowerCase()
+    }
+    if (clase == "si") {
+       
+        const precioTotal = compra.reduce((acc, el) => acc + el.precio + clasePremium - descuento2, 0)
+        alert("el valor de tu vuelo es de " + Math.round(precioTotal))
+    }
+    else {
+
+      
+        const precioTotal = compra.reduce((acc, el) => acc + el.precio - descuento, 0)
+        alert("el valor de tu vuelo es de " + Math.round(precioTotal))
+    }
 }
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+console.log(compra)
