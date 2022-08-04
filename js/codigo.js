8ddde06ba764af44b4799cfdfeb43c7138621078
SantiGennu <santigennu5@gmail.com>
2022-08-04 15:55:18 +0200

    libreria y otros ajustes

--- js/codigo.js
+++ js/codigo.js
@@ -1,3 +1,4 @@
+
 let carrito = []
 let contenedorDestinos = document.getElementById('contenedorDestinos')
 let contenedorCarrito = document.getElementById("contenedorCarrito")
@@ -13,19 +14,36 @@ function mostrarProductos(listaDestinos) {
         let div = document.createElement("div")
         div.className = "destino col-12 col-md-4 mt-3"
         div.innerHTML = `<div class="card" style="width: 18rem;">
-                                <img class="card-img-top" src="${item.img}">
-                            <div class="card-body">
-                                <h4 class="card-title">${item.ciudad} </h4>
-                                <p class= "precio">$ ${item.precio}</p>
-                                <button onclick="agregarCarrito(${item.id})" class="btn" id="botonProducto"><span class="material-icons">
-                                flight_takeoff
-                                </span></button>
-                            </div>
-                        </div>`
+        <img class="card-img-top" src="${item.img}">
+        <div class="card-body">
+        <h4 class="card-title">${item.ciudad} </h4>
+        <p class= "precio">$ ${item.precio}</p>
+        <button id= "${item.id}" class="btn"><span class="material-icons">
+        flight_takeoff
+        </span></button>
+        </div>
+        </div>`
         contenedorDestinos.appendChild(div)
+
+        let button = document.getElementById(`${item.id}`)
+        button.addEventListener("click", () => {
+            Swal.fire({
+                position: 'top-end',
+                icon: 'success',
+                title: 'Tu destino ha sido agregado al carrito',
+                showConfirmButton: false,
+                timer: 1500,
+            })
+            agregarCarrito(item)
+        })
+        mostrarCarrito()
+
+
+
 
     })
 }
+
 mostrarProductos(listaDestinos)
 mostrarOfertas()
 function mostrarOfertas() {
@@ -37,52 +55,86 @@ function mostrarOfertas() {
                             <div class="card-body">
                                 <h4 class="card-title">${item.ciudad} </h4>
                                 <p class= "precio">$ ${item.precio}</p>
-                                <button onclick="agregarCarrito(${item.id})" class="btn" id="botonProducto"><span class="material-icons">
+                                <button id= "${item.id}" class="btn" id="botonProducto"><span class="material-icons">
                                 flight_takeoff
                                 </span></button>
                             </div>
                         </div>`
 
         contenedorOfertas.appendChild(div)
+        let btn = document.getElementById(`${item.id}`)
+        btn.addEventListener("click", () => {
+            Swal.fire({
+                position: 'top-end',
+                icon: 'success',
+                title: 'Tu destino ha sido agregado al carrito',
+                showConfirmButton: false,
+                timer: 1500,
+            })
+            agregarCarrito(item)
+        })
+
     })
 }
 
-function agregarCarrito(idDestino) {
-    let destino = listaDestinos.find(destino => destino.id === idDestino)
+
+function agregarCarrito(item) {
+
+    let destino = listaDestinos.find(destino => destino.id === item.id)
     carrito.push(destino)
     localStorage.setItem("destino", JSON.stringify(carrito))
-console.log(destino)
+    console.log(destino)
+
     actualizarCarrito()
     console.log(carrito)
+
 }
+
+
 boton.addEventListener("click", mostrarCarrito)
 function mostrarCarrito() {
     contenedorCarrito.innerHTML = ""
 
-    carrito.forEach(item => {
+    if (carrito.length === 0) {
         let div = document.createElement("div")
-        div.className = "contenedorCarrito d-flex"
-        div.innerHTML = `<p class="col-6 align-items-end">${item.ciudad}</p>
-                        <p class="col-6 align-items-end">Precio: $${item.precio} </p>
-                        <button class="remove-btn"><span class="material-icons" id="button${item.id}">
-                        delete 
-                        </span></button>`
+        div.innerHTML = `<p id ="vacio" >EL CARRITO ESTA VACÍO</p>`
         contenedorCarrito.appendChild(div)
-        actualizarCarrito()
+    }
+    else {
 
-        const btnEliminar = document.getElementById(`button${item.id}`)
-        btnEliminar.addEventListener("click", () => {
-            btnEliminar.parentElement.parentElement.remove()
-            carrito.forEach(item => item.id == `button${item.id}`)
-            let index = carrito.indexOf(item)
-            carrito.splice(index, 1)
+        carrito.forEach(item => {
+            let div = document.createElement("div")
+            div.className = "contenedorCarrito d-flex"
+            div.innerHTML = `<p class="col-6 align-items-end">${item.ciudad}</p>
+                        <p class="col-6 align-items-end">Precio: $${item.precio} 
+                        <button id=booking class="booking-btn${item.id}"><span class="material-icons">
+                        flight_land
+                        </span></button></p>
+                        <button id="button${item.id}" class="remove-btn"><span class="material-icons" >
+                        delete 
+                        </span></button>
+                        `
+
+            contenedorCarrito.appendChild(div)
             actualizarCarrito()
 
 
+
+            const btnEliminar = document.getElementById(`button${item.id}`)
+            btnEliminar.addEventListener("click", () => {
+                btnEliminar.parentElement.remove()
+                carrito.forEach(item => item.id == `button${item.id}`)
+                let index = carrito.indexOf(item)
+                carrito.splice(index, 1)
+                actualizarCarrito()
+                mostrarCarrito()
+
+
+            })
+
+
         })
-
-
-    })
+    }
 }
 
 
@@ -91,16 +143,7 @@ function actualizarCarrito() {
     total.innerText = carrito.reduce((acc, el) => acc + el.precio, 0)
 }
 
-function borrarElemento(e) {
-    btn = e.target;
-    btn.parentElement.parentElement.remove();
-    const index = carrito.map(destino => destino.id == `button${destino.id}`).indexOf(e);
-    carrito.splice(index, 1);
-    actualizarCarrito()
-    console.log(e.target)
-
-}
-function filtrarDestino (e){
+function filtrarDestino(e) {
     let destinoFiltrado = listaDestinos.filter(destino => (destino.ciudad).toLowerCase().includes(e.target.value))
     mostrarProductos(destinoFiltrado)
     console.log(e.target.value)
@@ -108,3 +151,27 @@ function filtrarDestino (e){
 window.addEventListener("keyup", filtrarDestino)
 
 
+let pack = document.querySelectorAll(".contentPack")
+console.log(pack)
+
+// function mostrarPack() {
+//     listaPack.forEach(item => {
+//         let div = document.createElement("div")
+//         div.className = "titlePack"
+//         div.innerHTML = `<h1>${item.ciudad}</h1>
+//                     </div>
+//                     <div class="blockPack">
+//                         <P>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Alias ab ut perferendis inventore
+//                             suscipit, atque temporibus totam voluptatibus doloribus praesentium</P>
+//                         <p class="pricePack">
+//                             <strong> Only ${item.precio}</strong>
+//                         </p>
+//                         <button id= "${item.id}" onclick="agregarCarrito()" type="button"> RESERVE </button>
+//                     </div>`
+//         pack.appendChild(div)
+//         agregarCarrito(item)
+       
+//     })
+// }
+// actualizarCarrito()
+// mostrarPack()
--- js/destinos.js
+++ js/destinos.js
@@ -13,7 +13,7 @@ class Destinos {
 
 const destino1 = new Destinos("Roma", 500, "./img/ROMA.jpg", 1)
 const destino2 = new Destinos("Londres", 550, "./img/london.jpg", 2)
-const destino3 = new Destinos("Paris", 400, "./img/pexels-flo-dahm-699466.png", 3)
+const destino3 = new Destinos("ParÍs", 400, "./img/paris.jpg", 3)
 const destino4 = new Destinos("Amsterdam", 470, "../img/amsterdam.jpg", 4)
 const destino5 = new Destinos("Bruselas", 560, "../img/bruselas.jpg", 5)
 const destino6 = new Destinos("Viena", 580, "../img/viena.jpg", 6)
@@ -24,110 +24,32 @@ const destino10 = new Destinos("Lisboa", 510, "../img/lisboa.jpg", 10)
 const destino11 = new Destinos("Oslo", 600, "../img/oslo.jpg", 11)
 const destino12 = new Destinos("Madrid", 425, "../img/madrid.jpg", 12)
 const destino13 = new Destinos("Tirana", 355, "../img/tirana.jpg", 13)
-const destino14 = new Destinos("Sofia", 470, "../img/sofia.jpg", 14)
+const destino14 = new Destinos("SofÍa", 470, "../img/sofia.jpg", 14)
 const destino15 = new Destinos("Dublin", 500, "../img/dublin.jpg", 15)
 const destino16 = new Destinos("Turin", 420, "../img/turin.jpg", 16)
 const destino17 = new Destinos("Hamburgo", 550, "../img/hamburgo.jpg", 17)
 const destino18 = new Destinos("Mallorca", 610, "../img/Mallorca.jpg", 17)
 const destino19 = new Destinos("Marsella", 650, "../img/marsella.jpg", 19)
 const destino20 = new Destinos("Atenas", 575, "../img/atenas-8.jpg", 20)
+const destino21 = new Destinos("Venecia", 520, "../img/venecia1.jpg", 21)
+const destino22 = new Destinos("Barcelona", 480, "../img/barcelona.jpg", 22)
+const destino23 = new Destinos("Lyon", 540, "../img/lyon.jpeg", 23)
+const destino24 = new Destinos("PackSpain", 1150, "../img/lyon.jpeg",24)
+const destino25 = new Destinos("packFrance", 1239, "../img/lyon.jpeg",25)
+const destino26 = new Destinos("packItaly", 1300, "../img/lyon.jpeg",26)
+
+
 
 
 
 let listaDestinos = [destino1, destino2, destino3, destino4, destino5, destino6, destino7,
-    destino8, destino9, destino10, destino11,destino12,destino13,destino14,destino15,destino16,destino17,destino18,destino19,destino20];
-// const ciudades = listaDestinos.map(destino => destino.ciudad)
+    destino8, destino9, destino10, destino11,destino12,destino13,destino14,destino15, 
+    destino16,destino17,destino18,destino19,destino20, destino21, destino22, destino23];
+
 console.log(listaDestinos)
 
-let ofertaDestinos= [destino9, destino13, destino8]
+let ofertaDestinos= [destino9, destino13, destino8];
 console.log(ofertaDestinos)
 
-// const descuento = 49.99
-// const descuento2 = 99.99
-// const descuento3 = 149.99
-// const clasePremium = 99.99
-
-
-
-// const compra = []
-// const nombre = prompt("Ingrese nombre y apellido")
-// alert("Hola  " + nombre + ",  Bienvenido a Gate05")
-
-
-// bookingFlight()
-// function bookingFlight() {
-//     let ciudad = prompt("ingrese un destino europeo al que quiera visitar").toLocaleLowerCase()
-//     while (!ciudades.includes(ciudad)) {
-//         alert("no hay vuelos disponibles a ese destino")
-//         ciudad = prompt("ingrese un destino europeo al que quiera visitar").toLowerCase()
-
-
-//     }
-//     const city = listaDestinos.find(destino => destino.ciudad == ciudad)
-//     compra.push(city)
-
-//     let pack = prompt("desea elegir otro destino y aprovechar los mejores descuentos? (SI / NO)").toLowerCase()
-//     while (pack != "si" && pack != "no") {
-//         alert("respuesta incorrecta")
-//         pack = prompt("desea elegir otro destino y aprovechar los mejores descuentos? (SI / NO)").toLowerCase()
-//     }
-
-
-//     if (pack == "si") {
-//         bookingFlight()
-//     }
-//     else {
-//         let clase = prompt("desea viajar en clase premium? (SI = premium / NO = economic)").toLowerCase()
-
-//         while (clase != "si" && clase != "no") {
-//             alert("respuesta incorrecta")
-//             clase = prompt("desea viajar en clase premium? (SI = premium / NO = economic)").toLowerCase()
-//         }
-//         if (clase == "si") {
-
-//             const precioTotal = compra.reduce((acc, el) => acc + el.precio + clasePremium - descuento2, 0)
-//             alert("el valor de tu vuelo es de " + Math.round(precioTotal))
-//         }
-//         else {
-
-
-//             const precioTotal = compra.reduce((acc, el) => acc + el.precio - descuento, 0)
-//             alert("el valor de tu vuelo es de " + Math.round(precioTotal))
-//         }
-//     }
-    
-// }
-
-// function eliminarVuelo(eliminar) {
-//     const index = compra.map(destino => destino.ciudad).indexOf(eliminar)
-//     compra.splice(index, 1)
-// }
-// consultaEliminar()
-// function consultaEliminar() {
-//     if (compra.length > 0) {
-//         let ciudadCarrito = ""
-//         for (const viaje of compra) {
-//             ciudadCarrito += viaje.ciudad + ""
-//         }
-//         let consulta = prompt("el carrito actualmente tiene " + ciudadCarrito + "desea eliminar uno de estos? SI/NO")
-
-//         if (consulta == "si") {
-//             eliminar = prompt("que ciudad desea eliminar?")
-//             eliminarVuelo(eliminar)
-//             const precioTotal = compra.reduce((acc, el) => acc + el.precio + clasePremium - descuento, 0)
-//             alert("el precio es de" + precioTotal)
-//         }
-
-//         else {
-//             const precioTotal = compra.reduce((acc, el) => acc + el.precio + clasePremium - descuento2, 0)
-//         }
-        
-//     }
-
-//     else {
-//         alert("el carrito no tiene vuelos")
-//     }
-
-
-
-// }
+let listaPack =[ destino24, destino25, destino26]
+console.log(listaPack)
